0c69e3fa391963b0e366df500269c5fc1c7b12cb
Himalaya Gupta <guptahimalaya2@gmail.com>
2021-01-03 18:43:50 +0530

    first commit

---
+++ js/main.js
@@ -0,0 +1,18 @@
+var sidebarToggler = document.getElementsByClassName("sidebarToggler")[0];
+var sidebar = document.getElementsByClassName("mobileSidebar")[0];
+var cross = document.getElementsByClassName("cross")[0];
+// var i = sidebarToggler.getElementsByTagName('i')[0];
+sidebarToggler.addEventListener('click', function () {
+    sidebar.classList.toggle('sidebarActive');
+})
+cross.addEventListener('click', function () {
+    sidebar.classList.toggle('sidebarActive');
+})
+
+var bookNames = document.getElementsByClassName("bookName");
+
+for (i = 0; i < bookNames.length; i++) {
+    if (bookNames[i].innerText.length > 23 ) {
+        bookNames[i].innerText = bookNames[i].innerText.substring(0, 25) + ' ...';
+    }
+}
