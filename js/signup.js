1f062d4d8fcbe47f67bdaf88daf7a10070fa276d
Himalaya Gupta <guptahimalaya2@gmail.com>
2021-01-12 00:38:11 +0530

    profile page validation added

--- js/signup.js
+++ js/signup.js
@@ -1,87 +1,35 @@
-$(document).ready(function () {
-
-    var current_fs, next_fs, previous_fs; //fieldsets
-    var opacity;
-    var current = 1;
-    var steps = $("fieldset").length;
-
-    setProgressBar(current);
-
-    $(".next").click(function () {
-
-        current_fs = $(this).parent();
-        next_fs = $(this).parent().next();
-
-        //Add Class Active
-        $("#progressbar li").eq($("fieldset").index(next_fs)).addClass("active");
-
-        //show the next fieldset
-        next_fs.show();
-        //hide the current fieldset with style
-        current_fs.animate({
-            opacity: 0
-        }, {
-            step: function (now) {
-                // for making fielset appear animation
-                opacity = 1 - now;
-
-                current_fs.css({
-                    'display': 'none',
-                    'position': 'relative'
-                });
-                next_fs.css({
-                    'opacity': opacity
-                });
-            },
-            duration: 500
-        });
-        setProgressBar(++current);
-    });
-
-    $(".previous").click(function () {
-
-        current_fs = $(this).parent();
-        previous_fs = $(this).parent().prev();
-
-        //Remove class active
-        $("#progressbar li").eq($("fieldset").index(current_fs)).removeClass("active");
-
-        //show the previous fieldset
-        previous_fs.show();
-
-        //hide the current fieldset with style
-        current_fs.animate({
-            opacity: 0
-        }, {
-            step: function (now) {
-                // for making fielset appear animation
-                opacity = 1 - now;
-
-                current_fs.css({
-                    'display': 'none',
-                    'position': 'relative'
-                });
-                previous_fs.css({
-                    'opacity': opacity
-                });
-            },
-            duration: 500
-        });
-        setProgressBar(--current);
-    });
-
-    function setProgressBar(curStep) {
-        var percent = parseFloat(100 / steps) * curStep;
-        percent = percent.toFixed();
-        $(".progress-bar")
-            .css("width", percent + "%")
+var contact = document.getElementById('contact');
+var address = document.getElementById('address');
+var email = document.getElementById('email')
+var firstSection = document.getElementsByClassName('sectionFirst');
+var saveBtn = document.getElementById('saveBtn');
+var condition 
+function validateFirstSection(btn) {
+    for (i = 0; i < firstSection.length; i++) {
+        if (window.location.href.indexOf('profile') > -1){
+            condition = firstSection[i].value != '' && address.value.length >= 30 && contact.value.length > 9 && contact.value.length < 13 && isEmailOK();
+        }
+        else {
+            condition = firstSection[i].value != '' && address.value.length >= 30 && contact.value.length > 9 && contact.value.length < 13;
+        }
+        
+        if (condition) {
+            enableBtn(btn);
+        } else {
+            disableBtn(btn);
+        }
     }
+}
 
-    $(".submit").click(function () {
+function isEmailOK() {
+    if (email.value.includes('@') && (email.value.includes('.com') || email.value.includes('.mail'))) {
+        removeErrorMsg();
+        return true
+    } else {
+        displayErrorMsg('Email is not Valid');
         return false;
-    })
-
-});
+    }
+}
 
 function disableBtn(ele) {
     ele.disabled = true;
@@ -103,117 +51,198 @@ function removeErrorMsg() {
     document.getElementById('message').innerText = '';
 }
 
-var signupGenres = document.getElementsByClassName('signupGenre');
-var submitBtn = document.getElementsByClassName('submit')[0];
-var selectedGenre = document.getElementsByClassName('selected')
 
-disableBtn(submitBtn);
+// all the code above this will be used in both the pages ( profile.html and signup.html)
 
-for (i = 0; i < signupGenres.length; i++) {
-    signupGenres[i].addEventListener('click', (e) => {
-        var clickedGenre = e.target;
-        clickedGenre.classList.toggle('selected')
-        if (selectedGenre.length > 0) {
-            enableBtn(submitBtn)
-        } else {
-            disableBtn(submitBtn)
+
+
+
+if (window.location.href.indexOf('signup') > -1) {
+
+    $(document).ready(function () {
+        var current_fs, next_fs, previous_fs; //fieldsets
+        var opacity;
+        var current = 1;
+        var steps = $("fieldset").length;
+
+        setProgressBar(current);
+
+        $(".next").click(function () {
+
+            current_fs = $(this).parent();
+            next_fs = $(this).parent().next();
+
+            //Add Class Active
+            $("#progressbar li").eq($("fieldset").index(next_fs)).addClass("active");
+
+            //show the next fieldset
+            next_fs.show();
+            //hide the current fieldset with style
+            current_fs.animate({
+                opacity: 0
+            }, {
+                step: function (now) {
+                    // for making fielset appear animation
+                    opacity = 1 - now;
+
+                    current_fs.css({
+                        'display': 'none',
+                        'position': 'relative'
+                    });
+                    next_fs.css({
+                        'opacity': opacity
+                    });
+                },
+                duration: 500
+            });
+            setProgressBar(++current);
+        });
+
+        $(".previous").click(function () {
+
+            current_fs = $(this).parent();
+            previous_fs = $(this).parent().prev();
+
+            //Remove class active
+            $("#progressbar li").eq($("fieldset").index(current_fs)).removeClass("active");
+
+            //show the previous fieldset
+            previous_fs.show();
+
+            //hide the current fieldset with style
+            current_fs.animate({
+                opacity: 0
+            }, {
+                step: function (now) {
+                    // for making fielset appear animation
+                    opacity = 1 - now;
+
+                    current_fs.css({
+                        'display': 'none',
+                        'position': 'relative'
+                    });
+                    previous_fs.css({
+                        'opacity': opacity
+                    });
+                },
+                duration: 500
+            });
+            setProgressBar(--current);
+        });
+
+        function setProgressBar(curStep) {
+            var percent = parseFloat(100 / steps) * curStep;
+            percent = percent.toFixed();
+            $(".progress-bar")
+                .css("width", percent + "%")
         }
-    })
+
+        $(".submit").click(function () {
+            return false;
+        })
+
+    });
 
 
-}
+
+    var signupGenres = document.getElementsByClassName('signupGenre');
+    var submitBtn = document.getElementsByClassName('submit')[0];
+    var selectedGenre = document.getElementsByClassName('selected')
+
+    disableBtn(submitBtn);
+
+    for (i = 0; i < signupGenres.length; i++) {
+        signupGenres[i].addEventListener('click', (e) => {
+            var clickedGenre = e.target;
+            clickedGenre.classList.toggle('selected')
+            if (selectedGenre.length > 0) {
+                enableBtn(submitBtn)
+            } else {
+                disableBtn(submitBtn)
+            }
+        })
 
 
-// form validation starts now
-var firstSectionBtn = document.getElementById('sectionFirstBtn');
-var firstSection = document.getElementsByClassName('sectionFirst');
-var contact = document.getElementById('contact');
-var address = document.getElementById('address');
-
-
-disableBtn(firstSectionBtn);
-
-for (i = 0; i < firstSection.length; i++) {
-    firstSection[i].addEventListener('input', () => {
-        validateFirstSection();
-    })
-}
-
-function validateFirstSection() {
-    for (i = 0; i < firstSection.length; i++) {
-        if (firstSection[i].value != '' && address.value.length >= 30 && contact.value.length > 9 && contact.value.length < 13) {
-            enableBtn(firstSectionBtn);
-        } else {
-            disableBtn(firstSectionBtn);
-        }
     }
-}
+
+    // form validation starts now
+    var firstSectionBtn = document.getElementById('sectionFirstBtn');
+
+    disableBtn(firstSectionBtn);
+
+    for (i = 0; i < firstSection.length; i++) {
+        firstSection[i].addEventListener('input', () => {
+            validateFirstSection(firstSectionBtn);
+        })
+    }
+
+    var secondSectionBtn = document.getElementById('sectionSecondBtn');
+    var secondSection = document.getElementsByClassName('sectionSecond');
+    var password = document.getElementById('password');
+    var confirmPass = document.getElementById('confirmPassword');
+    var username = document.getElementById('username')
 
 
+    disableBtn(secondSectionBtn);
 
-var secondSectionBtn = document.getElementById('sectionSecondBtn');
-var secondSection = document.getElementsByClassName('sectionSecond');
-var password = document.getElementById('password');
-var confirmPass = document.getElementById('confirmPassword');
-var username = document.getElementById('username')
-var email = document.getElementById('email')
-
-disableBtn(secondSectionBtn);
-
-for (i = 0; i < secondSection.length; i++) {
-    secondSection[i].addEventListener('input', () => {
-        validateSecondSection();
-    })
-}
-
-var condition
-
-function validateSecondSection() {
     for (i = 0; i < secondSection.length; i++) {
-        condition = secondSection[i].value != '' && username.value.length >= 6 && password.value.length > 7 && confirmPass.value.length > 7;
-        if (condition) {
-            if (isPassowrdsEqual() && isEmailOK()) {
-                enableBtn(secondSectionBtn);
+        secondSection[i].addEventListener('input', () => {
+            validateSecondSection();
+        })
+    }
+
+
+    function validateSecondSection() {
+        for (i = 0; i < secondSection.length; i++) {
+            condition = secondSection[i].value != '' && username.value.length >= 6 && password.value.length > 7 && confirmPass.value.length > 7;
+            if (condition) {
+                if (isPassowrdsEqual() && isEmailOK()) {
+                    enableBtn(secondSectionBtn);
+                } else {
+                    disableBtn(secondSectionBtn);
+                }
             } else {
                 disableBtn(secondSectionBtn);
             }
+
+        }
+
+    }
+
+    function isPassowrdsEqual() {
+        if (password.value == confirmPass.value) {
+            removeErrorMsg();
+            return true;
         } else {
-            disableBtn(secondSectionBtn);
+            displayErrorMsg('Password Does Not Match')
+            return false;
         }
-
     }
+
+
+    var timer = document.getElementById('timer');
+    submitBtn.addEventListener('click', () => {
+        var timeleft = 0;
+        var downloadTimer = setInterval(function () {
+            if (timeleft > 4) {
+                clearInterval(downloadTimer);
+                window.location.replace("http://127.0.0.1:5500/index.html");
+            }
+            var remain = 5 - timeleft;
+            timer.innerText = 'Logging You in ' + remain + 's';
+            timeleft += 1;
+        }, 1000);
+    })
 
 }
 
-function isPassowrdsEqual() {
-    if (password.value == confirmPass.value) {
-        removeErrorMsg();
-        return true;
-    } else {
-        displayErrorMsg('Password Does Not Match')
-        return false;
+// validation for profile form is here 
+if (window.location.href.indexOf("profile") > -1) {
+    disableBtn(saveBtn);
+    validateFirstSection(saveBtn);
+    for (i = 0; i < firstSection.length; i++) {
+        firstSection[i].addEventListener('input', () => {
+            validateFirstSection(saveBtn);
+        })
     }
 }
-
-function isEmailOK() {
-    if (email.value.includes('@') && (email.value.includes('.com') || email.value.includes('.mail'))) {
-        removeErrorMsg();
-        return true
-    } else {
-        displayErrorMsg('Email is not Valid');
-        return false;
-    }
-}
-var timer = document.getElementById('timer');
-submitBtn.addEventListener('click', () => {
-    var timeleft = 0;
-    var downloadTimer = setInterval(function () {
-        if (timeleft > 4) {
-            clearInterval(downloadTimer);
-            window.location.replace("http://127.0.0.1:5500/index.html");
-        }
-        var remain = 5 - timeleft;
-        timer.innerText = 'Logging You in ' + remain + 's';
-        timeleft += 1;
-    }, 1000);
-})
